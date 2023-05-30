bd42507ef1d89eda8d34f2edfc74c44639abce51
Atman-Shastri <atmanshastri@gmail.com>
2023-05-30 20:38:34 +0530

    30-05-2023 08:38:34 PM

--- to_do_first.sh
+++ to_do_first.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cp .bash_aliases ~
 sudo apt update
-sudo apt-get install autojump curl xclip -y
+sudo apt-get install autojump curl wget xclip -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-linux-x86_64.sh
 cat << 'EOF' >> ~/.bashrc
 . /usr/share/autojump/autojump.sh
 export PATH="$PATH:/home/atman/workspace/ns-allinone-3.32/netanim-3.108/"
