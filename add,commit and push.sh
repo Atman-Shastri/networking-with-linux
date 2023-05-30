eed5bd17705d26cf868abd107ce6e617a3e25d12
Atman-Shastri <atmanshastri@gmail.com>
2023-05-30 13:21:02 +0530

    Tue May 30 13:21:02 IST 2023

--- add,commit and push.sh
+++ add,commit and push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-current_time=$(date +%R)
-am_pm=$(date +%p)
+DATE=$(date +"%dd-%mm-%yyyy")
+current_time=$(date +"%hh:%mm:%ss")
+am_pm=$(date +"%p")
 
 git config user.name "Atman-Shastri"
 git config user.email "atmanshastri@gmail.com"
