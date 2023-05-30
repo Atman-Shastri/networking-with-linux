01f86d7241ab05735c093ab99bd7a65821500872
Atman-Shastri <atmanshastri@gmail.com>
2023-05-30 18:55:36 +0530

    30-05-2023 06:55:36 PM

--- .bash_aliases
+++ .bash_aliases
@@ -1,1 +1,3 @@
 alias apti='sudo apt-get install'
+alias reload='. ~/.bashrc && exec bash'
+alias clip='xargs echo -n | xclip -selection clipboard'
--- to_do_first.sh
+++ to_do_first.sh
@@ -5,3 +5,6 @@ sudo apt update
 sudo apt-get install autojump -y
 echo '. /usr/share/autojump/autojump.sh' >> ~/.bashrc
 . ~/.bashrc
+echo 'export PATH=$PATH:/home/atman/workspace/ns-allinone-3.32/netanim-3.108/' >> ~/.bashrc
+. ~/.bashrc
+
