c89e17e6910d3c429c4627d6b18ecc3e81aa3c63
Atman-Shastri <atmanshastri@gmail.com>
2023-05-30 20:30:48 +0530

    30-05-2023 08:30:48 PM

--- to_do_first.sh
+++ to_do_first.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cp .bash_aliases ~
 sudo apt update
-sudo apt-get install autojump xclip -y
+sudo apt-get install autojump curl xclip -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 cat << 'EOF' >> ~/.bashrc
 . /usr/share/autojump/autojump.sh
 export PATH="$PATH:/home/atman/workspace/ns-allinone-3.32/netanim-3.108/"
@@ -10,5 +11,6 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 "(%s)")\$ '
 EOF
 . ~/.bashrc
+
 
 
