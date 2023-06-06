e38784ffd826071fda6b261e4b1123182e71bf77
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2023-06-06 10:51:27 +0530

    Updating to_do_first.sh

--- to_do_first.sh
+++ to_do_first.sh
@@ -7,6 +7,14 @@ alias reload='. ~/.bashrc && exec bash'
 alias clip='xargs echo -n | xclip -selection clipboard'
 alias nodemanin='curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash'
 alias pythonmanin='wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && sh ./Miniconda3-latest-Linux-x86_64.sh -b'
+alias wslshutdown='wsl.exe --terminate $WSL_DISTRO_NAME'
+alias wslreboot='cd /mnt/c/ && cmd.exe /c start "rebooting WSL" cmd /c "timeout 5 && wsl -d $WSL_DISTRO_NAME" && wsl.exe --terminate $WSL_DISTRO_NAME'
+alias remotedesktopins='sudo apt update
+curl -L -o chrome-remote-desktop_current_amd64.deb \
+    https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd>
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.d>
+rm -rf ./chrome-remote-desktop_current_amd64.deb'
 EOF
 sudo apt update
 sudo apt-get install autojump xdotool curl wget xclip -y
