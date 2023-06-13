e0cf1a7699d61593fc6ea82476693281f5985624
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2023-06-13 09:06:08 +0530

    Updating to_do_first.sh

--- to_do_first.sh
+++ to_do_first.sh
@@ -20,7 +20,7 @@ alias est='progress --monitor --pid $!'
 alias cp='rsync -ah --progress'
 EOF
 sudo apt update
-sudo apt-get install autojump xdotool curl wget xclip trash-cli -y
+sudo apt-get install autojump xdotool curl wget xclip trash-cli fzf -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh ./Miniconda3-latest-Linux-x86_64.sh -b
