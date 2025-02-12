ee94f9811eaea7cae1e09c88a2de9bfed2f16ce3
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2025-02-12 11:07:20 +0530

    Update to_do_first.sh

--- to_do_first.sh
+++ to_do_first.sh
@@ -29,6 +29,7 @@ alias rmls='trash-list'
 alias rmrs='trash-restore'
 alias rmemp='trash-empty -f'
 alias rmount="rclone mount LOS:laric_online_storage ~/LOS/ &"
+alias bash_alias="curl https://raw.githubusercontent.com/Atman-Shastri/networking-with-linux/refs/heads/main/to_do_first.sh"
 alias busy-ports="lsof -i -P -n | awk 'NR == 1 || /LISTEN/ {print}'"
 alias duplicate-services="sudo find /etc/systemd/system/ /lib/systemd/system/ /usr/lib/systemd/system/ -name '*.service' -print | sort | uniq -d"
 add_users() {
